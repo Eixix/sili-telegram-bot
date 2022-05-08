f7715deacdd81453b51c0e8cf09b31c3a02ef874
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-05-08 15:28:10 +0200

    add new method of verb duplication prevention

--- src/models/message.py
+++ src/models/message.py
@@ -1,5 +1,7 @@
 from models.matches import Matches
 import random
+import numpy as np
+import re
 import logging
 from models.match import Match
 from models.playerinfo import Playerinfo
@@ -8,8 +10,11 @@ import pytz
 class Message:
     matches = Matches
     punlines = {}
+    verb_numbers = {}
+    meme_const_cats = []
+    meme_const_cats_parsed = {}
     playerinfos = []
-    used_verbs = []
+    used_verbs = {}
 
     # this value represents the expected maximum number of players in a match
     verb_decay = 5
@@ -21,7 +26,11 @@ class Message:
     def __init__(self, matches, punlines,playerinfos):
         self.matches = matches
         self.punlines = punlines
+        self.verb_numbers = {key: len(value) for key, value in punlines['performance_verbs'].items()}
+        self.meme_const_cats = [key for key in punlines['performance_verbs'].keys()]
+        self.meme_const_cats_parsed = self._parse_meme_const_categories()
         self.playerinfos = playerinfos
+        self.used_verbs = self._reset_used_verbs()
         
     def get_messages_for_matches(self):
         messages = []
@@ -62,6 +71,8 @@ class Message:
 
             messages.append(f"{matchresult.name} hat mit {matchresult.hero} {self._generate_verb(matchresult)} mit {matchresult.kills} Kills, {matchresult.deaths} Toden und {matchresult.assists} Assists")
 
+        self._reset_used_verbs()
+
         return '\n\n'.join(messages)
         
     def _check_verb_used(self, verb):
@@ -71,19 +82,54 @@ class Message:
             used_verbs = used_verbs[1:self.verb_decay - 1] + [verb]
             return False
 
+    def _parse_meme_const_categories(self):
+        meme_const_cats_parsed = {}
+
+        for const_cat in self.meme_constant_cats:
+            prefix_match = re.search("^[<>]", const_cat)
+            cat_cat_match = re.search("(?<=^[<>])[0-9]+", const_cat)
+
+            if not all([prefix_match, cat_cat_match]):
+                raise(f"Could not parse meme constant category {const_cat}. "
+                        "Ensure it matches the convention of '[<>]x', with "
+                        "'x' being a positive integer.")
+
+            meme_const_cats_parsed[cat_cat_match.group(0)] = prefix_match.group(0)
+
+        self.meme_const_cats_parsed = meme_const_cats_parsed
+
+        return None
+
+    def _reset_used_verbs(self):
+        self.used_verbs = {key: [] for key in self.verb_numbers.keys()}
+
+        return None
+
     def _generate_verb(self, matchresult):
         verb = ""
-        if matchresult.meme_constant < 0.5:
-            verb = random.choice(self.punlines["performance_verbs"]["<0.5"])
-        elif matchresult.meme_constant < 1:
-            verb = random.choice(self.punlines["performance_verbs"]["<1"])
-        elif matchresult.meme_constant < 2:
-            verb = random.choice(self.punlines["performance_verbs"]["<2"])
-        elif matchresult.meme_constant < 5:
-            verb = random.choice(self.punlines["performance_verbs"]["<5"])
-        elif matchresult.meme_constant < 10:
-            verb = random.choice(self.punlines["performance_verbs"]["<10"])
+
+        mc_cat_array = np.array(self.meme_const_cats_parsed.key())
+
+        # TODO: this currently depends on categories being sorted, this may not be given
+        mc_cat_idx_arr = np.which(mc_cat_array > matchresult.meme_constant)[0]
+
+        if mc_cat_idx_arr.size == 0:
+            cat = self.meme_constant_cats[mc_cat_array.size - 1]
         else:
-            verb = random.choice(self.punlines["performance_verbs"][">10"])
+            cat = self.meme_constant_cats[mc_cat_array[0]]
+
+        cat_verb_n = self.verb_numbers[cat]
+
+        if len(self.used_verbs[cat]) >= cat_verb_n:
+            # TODO: log resetting of list of used indices
+            self._reset_used_verbs()
+
+        verb_idx_list = [* range(0, cat_verb_n)]
+
+        verb_idx_set_unused = set(verb_idx_list) - set(self.used_verbs[cat])
+
+        verb_idx = random.choice([* verb_idx_set_unused])
+
+        verb = self.punlines["performance_verbs"][cat][verb_idx]
 
         return verb
