9e4ebfee4af33f5cf4731d9eab90c8ea4567034e
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2022-05-13 19:28:12 +0200

    fix & improve url generation

--- src/models/voiceline.py
+++ src/models/voiceline.py
@@ -9,13 +9,13 @@ class Voiceline:
     soup = bs4.BeautifulSoup()
 
     def __init__(self, hero_string):
-        base_url = "https://dota2.fandom.com/wiki/"
+        base_url = "https://dota2.fandom.com/wiki"
 
         # On the fandom wiki the pages for heroes follow the pattern of 
         # "base_url/Capitalized_Hero/subpage", so we need to ensure the hero
         # name follows that pattern   
         hero_list = hero_string.split(" ")
-        self.hero = "_".join([x.upper() for x in hero_list])
+        self.hero = "_".join([x.capitalize() for x in hero_list])
 
         self.response_url = f"{base_url}/{self.hero}/Responses"
 
