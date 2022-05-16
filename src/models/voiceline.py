340cc1350c654e16cd5a8902d3510914c09c907e
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com[D>
2022-05-16 22:34:29 +0200

    format voiceline.py

--- src/models/voiceline.py
+++ src/models/voiceline.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 import regex
 
+
 class Voiceline:
     hero = ""
     response_url = ""
@@ -12,9 +13,9 @@ class Voiceline:
     def __init__(self, hero_string):
         base_url = "https://dota2.fandom.com/wiki"
 
-        # On the fandom wiki the pages for heroes follow the pattern of 
+        # On the fandom wiki the pages for heroes follow the pattern of
         # "base_url/Capitalized_Hero/subpage", so we need to ensure the hero
-        # name follows that pattern   
+        # name follows that pattern
         hero_list = hero_string.split(" ")
         self.hero = "_".join([x.capitalize() for x in hero_list])
 
@@ -22,9 +23,9 @@ class Voiceline:
 
         vl_pg_response = requests.get(self.response_url)
         self.soup = bs4.BeautifulSoup(vl_pg_response.content,
-                                        features = "html.parser")
+                                      features="html.parser")
 
-        # On the the wiki page all voiceline links are in tags which can be 
+        # On the the wiki page all voiceline links are in tags which can be
         # selected using this css selector.
         # TODO: check if this looks as expected (i.e. has content)
         self.vl_tags = self.soup.select(".mw-parser-output > ul li")
@@ -37,16 +38,16 @@ class Voiceline:
 
         for tag in self.vl_tags:
             # Each of the tags contains the audiobutton with the link to the
-            # audiofile as its first child and as its second child the text 
-            # of the voiceline. Here we find the tag containing the link to 
+            # audiofile as its first child and as its second child the text
+            # of the voiceline. Here we find the tag containing the link to
             # the desired line.
             # Note: Like this it will only perform basic string matching and
-            # stop at the first find. This may not be desirable if different 
+            # stop at the first find. This may not be desirable if different
             # lines have the same text (different intonation).
-            if re.search(line_re, tag.contents[-1]):
+            if regex.search(line_re, tag.contents[-1]):
                 line_tag = tag
                 break
-        
+
         if not line_tag:
             message = "Sorry, konnte die line nicht finden..."
         else:
