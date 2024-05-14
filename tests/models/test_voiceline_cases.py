c567dcbd55cca646673dfe19db8529c17e46d89c
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-05-14 23:07:47 +0200

    tests: Add voiceline link retrieval test

---
+++ tests/models/test_voiceline_cases.py
@@ -0,0 +1,33 @@
+import json
+import pytest
+
+from pytest_cases import parametrize
+
+
+def all_hero_names() -> list[str]:
+    """
+    Provide a list of all known hero names.
+    """
+    hero_info_path = "resources/dynamic/heroes.json"
+
+    with open(hero_info_path, "r") as f:
+        known_heroes = json.load(f)
+
+    return [hero["localized_name"] for hero in known_heroes]
+
+
+class TestVoicelineCases:
+    @pytest.mark.slow
+    @parametrize("hero_name", all_hero_names())
+    def case_hero_generator(self, hero_name: str):
+        """
+        Generate cases involving all known hero names.
+        """
+        return hero_name
+
+    @pytest.mark.duplication
+    def case_kotl(self):
+        """
+        Generate a case just for Keeper of the Light as a (almost) random hero.
+        """
+        return "Keeper of the Light"
