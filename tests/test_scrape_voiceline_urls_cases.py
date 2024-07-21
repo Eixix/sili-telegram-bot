f2201294fc42254bb9ea4c52871be33db4baec7d
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-07-21 16:58:52 +0200

    tests/vl_scraping: Add case for unicode ellipsis

--- tests/test_scrape_voiceline_urls_cases.py
+++ tests/test_scrape_voiceline_urls_cases.py
@@ -52,6 +52,9 @@ class TestVoicelineScraperCases:
 
         def case_followup(self):
             return ("Link▶️ Welcome to Dota. followup", "Welcome to Dota.")
+        
+        def case_ellipsis(self):
+            return ('Link▶️ Uh…ah…a chilling assault!', "Uh...ah...a chilling assault!")
 
     class TestResponseFromLinkTagCases:
         def case_basic(self):
