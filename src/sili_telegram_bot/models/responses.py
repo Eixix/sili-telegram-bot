bf37ddc435435071c6ed034cf4feee9526e1f153
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-08-25 20:10:43 +0200

    responses: Add voiceline methods
      This file will later replace voiceline.

--- src/sili_telegram_bot/models/responses.py
+++ src/sili_telegram_bot/models/responses.py
@@ -1,5 +1,7 @@
 import json
+import os
 import regex
+import requests
 
 from sili_telegram_bot.modules.config import config
 
@@ -87,3 +89,38 @@ class Responses:
                 f"Could not find line for '{name}'. Check the responses page to see "
                 f"if you typed it correctly: {response_url}"
             )
+
+    def get_link(self, hero, line):
+        fuzzy_rules = "{e<=1}"
+
+        if regex.search(r"^\".+\"", line):
+            line = line.strip('"')
+            line_re = regex.compile(line, flags=regex.IGNORECASE)
+
+        else:
+            line_re = regex.compile(
+                f"(?:{regex.escape(line)}){fuzzy_rules}", flags=regex.IGNORECASE
+            )
+
+        return self.get_response_url(name=hero, pattern=line_re)
+
+    def download_mp3(self, link):
+        file_name_match = regex.search(r"[^\/]*.mp3", link)
+
+        if not file_name_match:
+            raise (
+                f"Could not extract file name from link. Is the link "
+                f"correct? {link}"
+            )
+
+        file_path = os.path.join("resources", file_name_match.group())
+
+        dl_response = requests.get(link)
+
+        if not dl_response.status_code == 200:
+            raise (f"Could not get a positive response from {link}")
+
+        dl_file_handle = open(file_path, "wb")
+        dl_file_handle.write(dl_response.content)
+
+        return file_path
