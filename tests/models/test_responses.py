1908d226309ace7b4c1d9b02e67f5ff44de0e981
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-09-05 15:12:54 +0200

    tests: Adapt tests to response class

---
+++ tests/models/test_responses.py
@@ -0,0 +1,21 @@
+from pytest_cases import parametrize_with_cases
+
+import test_responses_cases as case_module
+
+from sili_telegram_bot.models.responses import Responses
+
+
+class TestResponses:
+    """
+    Test the Response class.
+    """
+
+    @parametrize_with_cases("hero_name", cases=case_module.TestResponsesCases)
+    def test_crummy_wizard_success(self, hero_name) -> None:
+        """
+        Test if the "Crummy wizard" voiceline can be retrieved for a given hero.
+        (That voiceline exists for every hero *so far*.)
+        """
+        rsp = Responses()
+        resp_url = rsp.get_link(hero_name, "Crummy wizard")
+        assert resp_url is not None
