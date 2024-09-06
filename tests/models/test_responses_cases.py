370d551ac506daf2a18f744683c78e784ff51e26
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-09-06 23:05:36 +0200

    test_responses: Add WIP entity tests

--- tests/models/test_responses_cases.py
+++ tests/models/test_responses_cases.py
@@ -38,3 +38,19 @@ class TestResponsesCases:
                 hero_name,
                 case_infra.first_voiceline(case_infra.hero_name_to_tile(hero_name)),
             )
+
+        @pytest.mark.slow
+        @pytest.mark.xfail(reason="Entity line getting not yet implemented.")
+        @parametrize(
+            "entity_name,entity_page_title",
+            zip(
+                case_infra.non_hero_entity_names(),
+                case_infra.non_hero_entity_page_titles(),
+            ),
+            ids=lambda input: input[0],
+        )
+        def case_remaining_entities(self, entity_name, entity_page_title):
+            return (
+                entity_name,
+                case_infra.first_voiceline(entity_page_title),
+            )
