6251158114013bb3455138b92258248104497172
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-09-08 13:07:38 +0200

    config: Factor creation into fun
      It was rather messy in that module before.

--- src/sili_telegram_bot/modules/config.py
+++ src/sili_telegram_bot/modules/config.py
@@ -9,33 +9,44 @@ import configparser as cfp
 
 _default_config_file = "./config/default.json"
 
-with open(_default_config_file, "r") as infile:
-    _default_config = json.load(infile)
 
-# Config values to be read from env. Keys represent the env var name, are len 2 lists
-# where the firt element specifies the section, the other the option name.
-_env_vars = _default_config["metaconfig"]["env_variables"]
+def _init_config(default_config_file) -> cfp.ConfigParser:
+    """
+    Initialize the CONFIG object by reading the default config, then the user config,
+    and then updating it with pre-defined variables from the environment.
+    """
+    with open(_default_config_file, "r") as infile:
+        default_config = json.load(infile)
 
-# Get the user-specific config file.
-_user_config_file = _default_config["metaconfig"]["user_config_file"]
+    # Config values to be read from env. Keys represent the env var name, are len 2 lists
+    # where the firt element specifies the section, the other the option name.
+    env_vars = default_config["metaconfig"]["env_variables"]
 
-# Remove metaconfig before initializing the object, it shouldn't be depended on for
-# anything other than this config bit.
-del _default_config["metaconfig"]
+    # Get the user-specific config file.
+    user_config_file = default_config["metaconfig"]["user_config_file"]
 
-config = cfp.ConfigParser(allow_no_value=True)
+    # Remove metaconfig before initializing the object, it shouldn't be depended on for
+    # anything other than this config bit.
+    del default_config["metaconfig"]
 
-# Set order of precedence, first default, then used, then env.
-config.read_dict(_default_config)
+    config = cfp.ConfigParser(allow_no_value=True)
 
-if os.path.exists(_user_config_file):
-    with open(_user_config_file, "r") as infile:
-        config.read_dict(json.load(infile))
+    # Set order of precedence, first default, then used, then env.
+    config.read_dict(default_config)
 
-for env_name, env_path in _env_vars.items():
-    update_value = os.environ.get(env_name)
+    if os.path.exists(user_config_file):
+        with open(user_config_file, "r") as infile:
+            config.read_dict(json.load(infile))
 
-    # Only update if the env var was set.
-    if update_value:
-        section = config[env_path[0]]
-        section[env_path[1]] = update_value
+    for env_name, env_path in env_vars.items():
+        update_value = os.environ.get(env_name)
+
+        # Only update if the env var was set.
+        if update_value:
+            section = config[env_path[0]]
+            section[env_path[1]] = update_value
+
+    return config
+
+
+config = _init_config(_default_config_file)
