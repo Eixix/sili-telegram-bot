bb4049b1c1239f8617003460245ba188b12a0f5a
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2024-09-09 17:26:29 +0200

    responses: Add level arg to get_link

--- src/sili_telegram_bot/models/responses.py
+++ src/sili_telegram_bot/models/responses.py
@@ -113,7 +113,7 @@ class Responses:
 
         return response_urls[level]
 
-    def get_link(self, entity, line, type="hero"):
+    def get_link(self, entity, line, type="hero", level=0):
         fuzzy_rules = "{e<=1}"
 
         if regex.search(r"^\".+\"", line):
@@ -125,7 +125,9 @@ class Responses:
                 f"(?:{regex.escape(line)}){fuzzy_rules}", flags=regex.IGNORECASE
             )
 
-        return self.get_response_url(name=entity, entity_type=type, pattern=line_re)
+        return self.get_response_url(
+            name=entity, entity_type=type, pattern=line_re, level=level
+        )
 
     def download_mp3(self, link):
         file_name_match = regex.search(r"[^\/]*.mp3", link)
